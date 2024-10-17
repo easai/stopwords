d6726bbbe28fbcee2d2d2b9dc1aa704337d93413
easai <aksarih@gmail.com>
2024-10-17 13:25:45 -0400

    add text results

--- stopwords/stopwords.py
+++ stopwords/stopwords.py
@@ -3,6 +3,7 @@ from matplotlib_venn import venn3
 import nltk
 import spacy
 import stopwordsiso
+import mplcursors
 
 # Get ISO English stopwords
 iso_stopwords = stopwordsiso.stopwords("en")
@@ -16,7 +17,19 @@ nltk.download('stopwords')
 nltk_stopwords = set(nltk.corpus.stopwords.words('english'))
 
 # Create a Venn diagram
-venn3([iso_stopwords, spacy_stopwords, nltk_stopwords], ('ISO stopwords', 'SpaCy stopwords', 'NLTK stopwords'))
+venn = venn3([iso_stopwords, spacy_stopwords, nltk_stopwords], ('ISO stopwords', 'SpaCy stopwords', 'NLTK stopwords'))
+
+# Define the sets for each section
+sets = {
+    '100': iso_stopwords - spacy_stopwords - nltk_stopwords,
+    '010': spacy_stopwords - iso_stopwords - nltk_stopwords,
+    '001': nltk_stopwords - iso_stopwords - spacy_stopwords,
+    '110': (iso_stopwords & spacy_stopwords) - nltk_stopwords,
+    '101': (iso_stopwords & nltk_stopwords) - spacy_stopwords,
+    '011': (spacy_stopwords & nltk_stopwords) - iso_stopwords,
+    '111': iso_stopwords & spacy_stopwords & nltk_stopwords
+}
+print(sets)
 
 # Display the plot
 plt.title("English stopwords")
